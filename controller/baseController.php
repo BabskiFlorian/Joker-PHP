2fb56856b69d33bbef7c0ab55cc0fb2f789c3392
BabskiFlorian <florianbabski@gmail.com>
2024-09-26 15:56:48 +0200

    first commit

---
+++ controller/baseController.php
@@ -0,0 +1,9 @@
+<?php
+    # initialiser des données sans html
+    const title = "Le Joker";
+    $date = date('d-m-Y');
+    $_dates = new DateTime();
+    $lang = ["fr","en"];
+    $navigator = $_SERVER["HTTP_USER_AGENT"];
+
+    # autres chose
